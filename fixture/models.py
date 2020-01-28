64e9f30b8884d781f140f1bcbda53b99e9bfb3cc
Rorywork <Rorywork95@outlook.com>
2020-01-28 21:09:06 +0000

    Implemented a Results Page

--- fixture/models.py
+++ fixture/models.py
@@ -2,13 +2,15 @@
 from django.db import models
 from django.shortcuts import render
 
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, FieldRowPanel
+from wagtail.core.fields import RichTextField
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
+from wagtail.core import blocks
 
-from streams import blocks
+#from streams import blocks
 
 
 class FixtureListingPage(RoutablePageMixin, Page):
@@ -41,6 +43,13 @@ class FixtureListingPage(RoutablePageMixin, Page):
 
 class FixtureDetailPage(Page):
 	"""Fixture detail page."""
+
+	competition = models.CharField(
+		max_length=100,
+		blank=False,
+		null=True,
+		help_text='Name of Competition e.g. Div 2, Cup QF etc.',
+	)
 
 	hometeam = models.CharField(
 		max_length=200,
@@ -90,17 +99,40 @@ class FixtureDetailPage(Page):
 		max_length=100,
 		blank=False,
 		null=False,
-		help_text='Fixture status',
+		help_text='Fixture status- set to either Upcoming Match or Result',
 	)
 
+	HT_FT_PTS = models.IntegerField(null=True, blank=True)
+	AT_FT_PTS = models.IntegerField(null=True, blank=True)
+	HT_HT_PTS = models.IntegerField(null=True, blank=True)
+	AT_HT_PTS = models.IntegerField(null=True, blank=True)
+
+	Home_Team_Scorers = RichTextField(features=["bold", "italic"],null=True, blank=True)
+	Away_Team_Scorers = RichTextField(features=["bold", "italic"], null=True, blank=True)
+
+	Match_Officials = RichTextField(features=["bold", "italic"],null=True, blank=True)
+	Match_Report = RichTextField(features=["bold", "italic"],null=True, blank=True)
 
 	content_panels = Page.content_panels + [
+		FieldPanel("competition"),
 		FieldPanel("hometeam"),
 		ImageChooserPanel("hometeam_image"),
 		FieldPanel("awayteam"),
 		ImageChooserPanel("awayteam_image"),
 		FieldPanel("fixture_date_time"),
 		FieldPanel("fixture_location"),
-		FieldPanel("fixture_status")
+		FieldPanel("fixture_status"),
+		FieldRowPanel(
+			[
+				FieldPanel("HT_FT_PTS"),
+				FieldPanel("HT_HT_PTS"),
+				FieldPanel("AT_FT_PTS"),
+				FieldPanel("AT_HT_PTS"),
+			]
+		),
+		FieldPanel("Home_Team_Scorers"),
+		FieldPanel("Away_Team_Scorers"),
+		FieldPanel("Match_Officials"),
+		FieldPanel("Match_Report")
 	]
 
